78cc381abcd78663a15728ebcb3b369082b6f747
Hohol <nikita.glashenko@gmail.com>
2017-08-11 02:21:14 +0500

    fignya

--- src/fidel/interaction/GameStateReader.java
+++ src/fidel/interaction/GameStateReader.java
@@ -299,7 +299,7 @@ public class GameStateReader {
 //        BufferedImage img = gameStateReader.getImageFromCapture();
         BufferedImage[][] tileImages = gameStateReader.getTileImages(img);
 
-        gameStateReader.saveTile(tileImages[1][4], ALIEN);
+        gameStateReader.saveTile(tileImages[4][6], ROBO_MEDIKIT);
 
         /*BufferedImage img = new GameStateReader().getImageFromCapture();
         int cnt = 1;
